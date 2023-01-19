37a554425e2458d488ec89c7215b9005c5cc879e
stevengonzalez <stevengonzalez@LEM231-15>
2023-01-18 20:10:19 -0800

    Add project files.

---
+++ COMP003AAssignment1/Program.cs
@@ -0,0 +1,118 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * 
+ *  Author:  Steven Gonzalez
+ *  Course:  COMP-003A-L01
+ *  Purpose: Code for Assignment 1
+ */  
+namespace COMP003AAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("100 things about me");
+            Console.WriteLine("1. first name steven");
+            Console.WriteLine("2. last name gonzalez");
+            Console.WriteLine("3. favorite number 9");
+            Console.WriteLine("4. favorite color purple");
+            Console.WriteLine("5. 1 cat");
+            Console.WriteLine("6. 1 dog");
+            Console.WriteLine("7. cat name tobi");
+            Console.WriteLine("8. dog name spike");
+            Console.WriteLine("9. hate spiders");
+            Console.WriteLine("10. I like sharks");
+            Console.WriteLine("11. bugs");
+            Console.WriteLine("12. I have 6 brothers");
+            Console.WriteLine("13. I have 1 sister");
+            Console.WriteLine("14. I have sister April");
+            Console.WriteLine("15. I have brother jose");
+            Console.WriteLine("16. I have brother Ernesto");
+            Console.WriteLine("17. I have brother Danial");
+            Console.WriteLine("18. I have brother Rene");
+            Console.WriteLine("19. I have brother Filex");
+            Console.WriteLine("20. I have dad");
+            Console.WriteLine("21. I have mom");
+            Console.WriteLine("22. I have cousins");
+            Console.WriteLine("23. I have a house");
+            Console.WriteLine("24. I have a pc");
+            Console.WriteLine("25. I have video games");
+            Console.WriteLine("26. I have xbox");
+            Console.WriteLine("27. I have ps4");
+            Console.WriteLine("28. I have a tv");
+            Console.WriteLine("29. I have remote");
+            Console.WriteLine("30. I have Hulu");
+            Console.WriteLine("31. I have 3 classes");
+            Console.WriteLine("32. I have friends");
+            Console.WriteLine("33. I have friend chris");
+            Console.WriteLine("34. I have friend jessie");
+            Console.WriteLine("35. I have friend dakota");
+            Console.WriteLine("36. I have friend adan");
+            Console.WriteLine("37. I have friend andrew");
+            Console.WriteLine("38. I have friend jeremiah");
+            Console.WriteLine("39. I have friend natasha");
+            Console.WriteLine("40. I have friend juliana");
+            Console.WriteLine("41. I have friend star");
+            Console.WriteLine("42. I have friend jon");
+            Console.WriteLine("43. I have a car");
+            Console.WriteLine("44. I have a mitsubishi");
+            Console.WriteLine("45. I have a iphone 14");
+            Console.WriteLine("46. I have alays lived in hanford");
+            Console.WriteLine("47. I like anime");
+            Console.WriteLine("48. I like the anime naruto");
+            Console.WriteLine("49. I like the anime one piece");
+            Console.WriteLine("50. I like the anime 7 deadly sins");
+            Console.WriteLine("51. I like the anime death note");
+            Console.WriteLine("52. I like the anime mob psycho");
+            Console.WriteLine("53. I like the anime HXH");
+            Console.WriteLine("54. I like the anime My hero");
+            Console.WriteLine("55. I like the anime blue lock");
+            Console.WriteLine("56. I like the anime dragon ball");
+            Console.WriteLine("57. I like the anime bleach");
+            Console.WriteLine("58. I watch movies");
+            Console.WriteLine("59. favorite movie is now you see me");
+            Console.WriteLine("60. I love movies");
+            Console.WriteLine("61. Movie I like is 300");
+            Console.WriteLine("62. Movie I like is chronicles");
+            Console.WriteLine("63. Movie I like is silent voice");
+            Console.WriteLine("64. Movie I like is now you see me 2");
+            Console.WriteLine("65. Movie I like is top gun");
+            Console.WriteLine("66. Movie I like is puss in boots");
+            Console.WriteLine("67. Movie I like is project x");
+            Console.WriteLine("68. Movie I like is split");
+            Console.WriteLine("69. Movie I like is hush");
+            Console.WriteLine("70. Movie I like is old");
+            Console.WriteLine("71. I play a lot of video games");
+            Console.WriteLine("72. favorite cod game is MW2");
+            Console.WriteLine("73. I play overwatch");
+            Console.WriteLine("74. I play apex");
+            Console.WriteLine("75. I play warzone");
+            Console.WriteLine("76. I play fortnite");
+            Console.WriteLine("77. I play valorant");
+            Console.WriteLine("78. I play clash of clans");
+            Console.WriteLine("79. I play clash royal");
+            Console.WriteLine("08. I play pokemon go");
+            Console.WriteLine("81. I play solitare");
+            Console.WriteLine("82. I play soccer");
+            Console.WriteLine("83. I play vollyball");
+            Console.WriteLine("84. I play ping pong");
+            Console.WriteLine("85. I play with friends");
+            Console.WriteLine("86. I play with family");
+            Console.WriteLine("87. I play on my pc");
+            Console.WriteLine("88. I play on my xbox");
+            Console.WriteLine("89. I play on my switch");
+            Console.WriteLine("90. I playon my ps4");
+            Console.WriteLine("91. I care about my family");
+            Console.WriteLine("92. I care about my friends");
+            Console.WriteLine("93. I have a Girlfriend");
+            Console.WriteLine("94. girlfriend name is natasha");
+            Console.WriteLine("95. bestfriend name is jessie");
+            Console.WriteLine("96. favorite show is atipichle");
+            Console.WriteLine("97. I built my own pc");
+            Console.WriteLine("98. I love tech");
+            Console.WriteLine("99. I like fixing things");
+            Console.WriteLine("100. like collecting rocks");
+
+        }
+    }
+}
